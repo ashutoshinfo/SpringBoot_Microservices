05b2eb9342a09cfef672c67b8343ba92b5835106
Ashutosh <ashutoshpatel@yahoo.in>
2022-11-26 14:24:03 +0530

    Fix: Project Setup

--- 001_Scheduling_CoreJava/src/main/java/info/ashutosh/App.java
+++ 001_Scheduling_CoreJava/src/main/java/info/ashutosh/App.java
@@ -1,13 +1,22 @@
 package info.ashutosh;
 
+import java.util.Timer;
+
+import info.ashutosh.job.Task1;
+import info.ashutosh.job.Task2;
+
 /**
- * Hello world!
+ * 
+ * @author ashutoshjp
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		Timer timer = new Timer();
+		timer.schedule(new Task1(), 3000, 2000);
+
+		Timer timer2 = new Timer();
+		timer2.schedule(new Task2(), 3000, 2000);
+	}
 }
