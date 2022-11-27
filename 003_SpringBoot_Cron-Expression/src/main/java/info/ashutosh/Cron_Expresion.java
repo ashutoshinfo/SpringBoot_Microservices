2edac37c0a5f6cfaa05e4fef3887905b17e481ed
Ashutosh <ashutoshpatel@yahoo.in>
2022-11-27 22:05:11 +0530

    Feat: Cron-Expression

---
+++ 003_SpringBoot_Cron-Expression/src/main/java/info/ashutosh/Cron_Expresion.java
@@ -0,0 +1,15 @@
+package info.ashutosh;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cron_Expresion {
+
+	@Scheduled(cron = "40-59,0-10 * * * * *")
+	public void work() {
+		// task execution logic
+		System.out.println("Cron_Expresion.work()");
+
+	}
+}
