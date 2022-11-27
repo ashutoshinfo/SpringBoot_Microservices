a2e35cf909e98f0881aaeaaf87312636dccaab7b
Ashutosh <69671369+ashutoshjp@users.noreply.github.com>
2022-11-27 22:23:23 +0530

    Update Application.java
    Create conflict intentionally

--- 003_SpringBoot_Cron-Expression/src/main/java/info/ashutosh/Application.java
+++ 003_SpringBoot_Cron-Expression/src/main/java/info/ashutosh/Application.java
@@ -9,7 +9,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);		
+		SpringApplication.run(Application.class, args);	
+		//live
 	}
 
 }
