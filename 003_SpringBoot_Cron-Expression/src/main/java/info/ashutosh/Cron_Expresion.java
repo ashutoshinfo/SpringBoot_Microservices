5a1ae50112b9f031741f97548947d2cfa859b6a8
Ashutosh <ashutoshpatel@yahoo.in>
2022-11-27 23:20:33 +0530

    FIx

--- 003_SpringBoot_Cron-Expression/src/main/java/info/ashutosh/Cron_Expresion.java
+++ 003_SpringBoot_Cron-Expression/src/main/java/info/ashutosh/Cron_Expresion.java
@@ -10,10 +10,6 @@ public class Cron_Expresion {
 	public void work() {
 		// task execution logic
 		System.out.println("Cron_Expresion.work()");
-<<<<<<< HEAD
-		// local 1
-=======
 		// Live 1
->>>>>>> branch 'master' of https://github.com/ashutoshjp/SpringBoot_Microservices.git
 	}
 }
