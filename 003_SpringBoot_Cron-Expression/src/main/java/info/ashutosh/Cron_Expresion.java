34622a068b857804730170207c695bf7976cfdbc
Ashutosh <ashutoshpatel@yahoo.in>
2022-11-28 22:42:13 +0530

    Feat: Cron Excretions

--- 003_SpringBoot_Cron-Expression/src/main/java/info/ashutosh/Cron_Expresion.java
+++ 003_SpringBoot_Cron-Expression/src/main/java/info/ashutosh/Cron_Expresion.java
@@ -6,6 +6,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class Cron_Expresion {
 
+	// * - any/ all/ every
+	// / - To specify Period of Time
+	// , - To specify the Posible list of value
+	// - - To specify range of values
+	// ? - any (can be used only in date and week day when month is secified)
+	// L - To specify last days info (can be used only in Date and weeks day fields)
+	// W - To specify Week day info (can be used inly in Date and Week day fields)
+	// @ - To work with @yearly, @hourly, @monthly, @daily and etc..
+	// # - As a combination symbol
 	@Scheduled(cron = "40-59,0-10 * * * * *")
 	public void work() {
 		// task execution logic
